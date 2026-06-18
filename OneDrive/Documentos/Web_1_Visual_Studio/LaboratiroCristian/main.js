08bd625a79bccbb076571e528dfd2589dcd1db65
CristianAguilar <aguilarvelacristianjhoel@gmail.com>
2026-06-18 13:30:21 -0500

    He creado 2 paginas que son de html y js y cumpli con los requerimientos del laboratorio dados por el ingeniero Eder

---
+++ OneDrive/Documentos/Web_1_Visual_Studio/LaboratiroCristian/main.js
@@ -0,0 +1,58 @@
+//fase 1
+console.log("Iniciando laboratorio...");
+
+// fase 2
+let edad = 20;
+let esEstudiante = true;
+console.log(typeof edad);
+console.log(typeof esEstudiante);
+
+// fase 3
+let nombre = "Cristian";
+let saludo = `Hola ${nombre}, tienes ${edad} años.`;
+
+console.log(saludo);
+
+let mensaje = `
+Bienvenido ${nombre}
+Curso: JavaScript
+Edad: ${edad}
+`;
+
+console.log(mensaje);
+
+// fase 4
+let sabor = "chocolate";
+
+if (sabor === "chocolate") {
+    console.log("Elegiste chocolate");
+} else {
+    console.log("Elegiste otro sabor");
+}
+
+let opcion = 2;
+
+switch(opcion){
+
+    case 1:
+        console.log("Opción 1: Registrar");
+        break;
+
+    case 2:
+        console.log("Opción 2: Consultar");
+        break;
+
+    case 3:
+        console.log("Opción 3: Eliminar");
+        break;
+
+    default:
+        console.log("Opción no válida");
+}
+
+// fase 5
+for(let i = 0; i < 10; i++){
+
+    console.log(`Iteración número: ${i}`);
+
+}
